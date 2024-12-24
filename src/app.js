ed092d6c3e59b3239b9f18947af2569b0c6d8bcc
pushpiinder <pushpinder1997.ps@gmail.com>
2024-12-24 13:10:57 +0530

    ep-04 part1

--- src/app.js
+++ src/app.js
@@ -2,16 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hello", (req, res) => {
-  res.send("Hello hello hello!");
+// This will only handle GET call to /user
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Akshay", lastName: "Saini" });
 });
 
+app.post("/user", async (req, res) => {
+  console.log(req.body);
+  // saving data to DB
+  res.send("Data successfully saved to the database!");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Deleted successfully!");
+});
+
+// this will match all the HTTP method API calls to /test
 app.use("/test", (req, res) => {
   res.send("Hello from server!");
-});
-
-app.use("/", (req, res) => {
-  res.send("Hello from dashboard!");
 });
 
 app.listen(3000, () => {
