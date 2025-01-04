c58a71dfcd3d063b2c3fe9dd8a6fb178a759cf24
pushpiinder <pushpinder1997.ps@gmail.com>
2025-01-04 22:43:57 +0530

    debug-1

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,15 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://namastedev:ECHLEON45456@namastenode.2f8gt.mongodb.net/devTinder"
-  );
+  try {
+    await mongoose.connect(
+      "mongodb+srv://namastedev:ECHLEON45456@namastenode.2f8gt.mongodb.net/devTinder"
+    );
+    console.log("MongoDB Connected...");
+  } catch (err) {
+    console.error("MongoDB connection error:", err.message);
+    throw err;
+  }
 };
 
 module.exports = connectDB;
